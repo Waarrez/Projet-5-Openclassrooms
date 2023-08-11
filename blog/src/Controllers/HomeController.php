0e74c51cf117026bbeb006aa43939a8ec1d6d9c8
Warez-amasya <thimote.cabotte6259@gmail.com>
2023-08-11 12:55:23 +0200

    add login and register method

--- blog/src/Controllers/HomeController.php
+++ blog/src/Controllers/HomeController.php
@@ -3,19 +3,26 @@
 namespace Zitro\Blog\Controllers;
 
 use BDD;
+use JetBrains\PhpStorm\NoReturn;
+use Mail;
+use Exception;
 use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
 use Twig\Loader\FilesystemLoader;
+use function RectorPrefix202307\dump;
+use function RectorPrefix202307\Symfony\Component\DependencyInjection\Loader\Configurator\expr;
 
 class HomeController
 {
 
     private $bdd;
+    private $mail;
 
-    public function __construct(BDD $bdd) {
+    public function __construct(BDD $bdd, Mail $mail) {
         $this->bdd = $bdd;
+        $this->mail = $mail;
     }
 
     /**
@@ -26,14 +33,32 @@ class HomeController
     public function index(): void
     {
         $loader = new FilesystemLoader('../src/templates');
-        $twig = new Environment($loader);
+        $twig = new Environment($loader, [
+            'cache' => false,
+            'strict_variables' => false,
+            'debug' => true,
+        ]);
+
+        $twig->addExtension(new \Twig\Extension\DebugExtension());
+
+        $user = null;
 
         // Chargement du template
         $template = $twig->load('pages/accueil.twig');
+        session_start();
+
+        if (isset($_SESSION["user_id"])) {
+            $idUser = $_SESSION["user_id"];
+
+            $request = $this->bdd->query("SELECT * FROM user WHERE id = '$idUser'");
+
+            $user = $request->fetch_assoc();
+        }
 
         // Affichage du template
         echo $template->render([
             'titre' => 'Mon Blog | Accueil',
+            'users' => $user
         ]);
     }
 
@@ -98,28 +123,100 @@ class HomeController
         // Chargement du template
         $template = $twig->load('pages/register.twig');
 
-        // Affichage du template
-        echo $template->render([
-            'titre' => 'Mon Blog | Inscription',
-        ]);
+        // Définir la variable "titre" utilisée dans le fichier "base.html.twig"
+        $titre = "Page d'inscription";
+
+        // Afficher le template rendu avec la variable "titre"
+        echo $template->render(['titre' => $titre]);
     }
 
     /**
-     * @throws SyntaxError
-     * @throws RuntimeError
-     * @throws LoaderError
+     * @throws Exception
      */
-    public function test(): void
+    public function addUser(): void
     {
+        if($_SERVER["REQUEST_METHOD"] === "POST") {
+            $email = $_POST["email"];
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            $confirmPassword = $_POST["confirmPassword"];
+            $roles = "ROLE_USER";
+            $confirmAccount  = bin2hex(random_bytes(32));
+
+            $hashPassword = password_hash($password, PASSWORD_ARGON2I);
+
+            if(!empty($email) && !empty($username) && !empty($password) && !empty($confirmPassword)) {
+                $verifyEmail = $this->bdd->query("SELECT * FROM user WHERE email = '$email'");
+
+               if($verifyEmail->fetch_all() !== []) {
+                   echo "Email deja utilisé";
+               } else {
+                   if($password !== $confirmPassword) {
+                       echo "Vos mots de passes doivent correspondre";
+                   } else {
+                       $result = $this->bdd->query("INSERT INTO user (email,username,password, confirmAccount , roles) VALUES ('$email', '$username', '$hashPassword', '$confirmAccount' ,'$roles')");
+
+                       if($result === TRUE) {
+                            var_dump($this->mail->sendMail($email, $username, $confirmAccount));
+                       } else {
+                           echo "Erreur lors de l'inscription";
+                       }
+                   }
+               }
+            }
+        }
+
         $loader = new FilesystemLoader('../src/templates');
         $twig = new Environment($loader);
 
         // Chargement du template
-        $template = $twig->load('pages/test.html.twig');
+        $template = $twig->load('pages/register.twig');
 
-        // Affichage du template
-        echo $template->render([
-            'titre' => 'Mon Blog | Inscription',
-        ]);
+        // Définir la variable "titre" utilisée dans le fichier "base.html.twig"
+        $titre = "Page d'inscription";
+
+        // Afficher le template rendu avec la variable "titre"
+        echo $template->render(['titre' => $titre]);
+    }
+
+
+    public function loginUser(): void
+    {
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+
+            $stmt = $this->bdd->query("SELECT * FROM user WHERE email = '$email'");
+
+            if ($stmt->num_rows === 1) {
+                $row = $stmt->fetch_assoc();
+                $userId = $row["id"];
+                $hashPasswordFromDatabase = $row["password"];
+
+                // Vérifier le mot de passe
+                if (password_verify($password, $hashPasswordFromDatabase)) {
+                    session_start();
+                    $_SESSION["user_id"] = $userId; // Stocker l'ID de l'utilisateur dans la session
+                    header("Location: /"); // Rediriger vers la page du tableau de bord après la connexion
+                } else {
+                    // Mot de passe incorrect
+                    echo "Mot de passe incorrect.";
+                }
+            } else {
+                // L'utilisateur n'existe pas
+                echo "Utilisateur non trouvé.";
+            }
+
+            $stmt->close();
+        }
+    }
+
+    public function logout(): void
+    {
+        session_start();
+
+        session_destroy();
+
+        header("Location: /");
     }
 }
