b9ed9ad7cec93a1f5247e602e51d898f93411075
Warez-amasya <thimote.cabotte6259@gmail.com>
2023-08-18 12:48:45 +0200

    function add article

--- blog/public/index.php
+++ blog/public/index.php
@@ -12,6 +12,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
+
 $servername = "localhost:3307";
 $username = "root";
 $password = "";
@@ -27,8 +29,11 @@ $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) 
     $r->addRoute('GET', '/login', [HomeController::class, 'login']);
     $r->addRoute('GET', '/register', [HomeController::class, 'register']);
     $r->post('/register', [HomeController::class, 'addUser']);
+    $r->get('/article/add', [HomeController::class, 'addArticle']);
+    $r->post('/article/add', [HomeController::class, 'addArticle']);
     $r->post('/login', [HomeController::class, 'loginUser']);
     $r->get('/logout', [HomeController::class, 'logout']);
+    $r->get('/article/{id:\d+}', [HomeController::class, 'getArticle']);
 });
 
 $httpMethod = filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_SPECIAL_CHARS);
--- blog/src/Controllers/HomeController.php
+++ blog/src/Controllers/HomeController.php
@@ -45,7 +45,6 @@ class HomeController
 
         // Chargement du template
         $template = $twig->load('pages/accueil.twig');
-        session_start();
 
         if (isset($_SESSION["user_id"])) {
             $idUser = $_SESSION["user_id"];
@@ -69,12 +68,23 @@ class HomeController
      */
     public function articles(): void
     {
+
         $loader = new FilesystemLoader('../src/templates');
         $twig = new Environment($loader, [
             'cache' => false,
             'strict_variables' => false,
             'debug' => true,
         ]);
+
+        $user = null;
+
+        if (isset($_SESSION["user_id"])) {
+            $idUser = $_SESSION["user_id"];
+
+            $request = $this->bdd->query("SELECT * FROM user WHERE id = '$idUser'");
+
+            $user = $request->fetch_assoc();
+        }
 
         $twig->addExtension(new \Twig\Extension\DebugExtension());
 
@@ -87,7 +97,40 @@ class HomeController
         // Affichage du template
         echo $template->render([
             'titre' => 'Mon Blog | Articles',
-            'article' => $articles
+            'article' => $articles,
+            'users' => $user
+        ]);
+    }
+
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
+    public function getArticle($id): void
+    {
+        $loader = new FilesystemLoader('../src/templates');
+        $twig = new Environment($loader, [
+            'cache' => false,
+            'strict_variables' => false,
+            'debug' => true,
+        ]);
+
+        $article = null;
+
+        foreach ($id as $identifiant) {
+            $result = $this->bdd->query("SELECT * FROM article WHERE id = $identifiant");
+            $article = $result->fetch_assoc();
+        }
+
+        $twig->addExtension(new \Twig\Extension\DebugExtension());
+
+
+        $template = $twig->load('pages/article.twig');
+
+        echo $template->render([
+            'titre' => 'Mon Blog | Articles',
+            'article' => $article
         ]);
     }
 
@@ -179,6 +222,39 @@ class HomeController
         echo $template->render(['titre' => $titre]);
     }
 
+    public function addArticle(): void
+    {
+        $loader = new FilesystemLoader('../src/templates');
+        $twig = new Environment($loader);
+
+        // Chargement du template
+        $template = $twig->load('pages/add_article.twig');
+
+        // Définir la variable "titre" utilisée dans le fichier "base.html.twig"
+        $titre = "Ajouter un article";
+
+
+
+        if([$_SERVER["REQUEST_METHOD"] === "POST"]) {
+            if (isset($_POST["title"]) && isset($_POST["content"])) {
+                $title = $_POST["title"];
+                $content = $_POST["content"];
+                $publishedAt = new \DateTimeImmutable();
+                $format = $publishedAt->format('Y-m-d');
+                session_start();
+                $authorId = $_SESSION["user_id"];
+
+                $this->bdd->query("INSERT INTO article (title, content, publishedAt, author_id) VALUES ('$title', '$content', '$format', '$authorId')");
+                header("Location: /"); // Correction ici
+                exit(); // Assurez-vous de quitter le script après la redirection
+                // Maintenant vous pouvez utiliser $title et $content en toute sécurité
+            }
+        }
+
+        // Afficher le template rendu avec la variable "titre"
+        echo $template->render(['titre' => $titre]);
+    }
+
 
     public function loginUser(): void
     {
@@ -195,9 +271,13 @@ class HomeController
 
                 // Vérifier le mot de passe
                 if (password_verify($password, $hashPasswordFromDatabase)) {
-                    session_start();
-                    $_SESSION["user_id"] = $userId; // Stocker l'ID de l'utilisateur dans la session
-                    header("Location: /"); // Rediriger vers la page du tableau de bord après la connexion
+                    if($row["confirmAccount"] !== null) {
+                        session_start();
+                        $_SESSION["user_id"] = $userId; // Stocker l'ID de l'utilisateur dans la session
+                        header("Location: /"); // Rediriger vers la page du tableau de bord après la connexion
+                    } else {
+                        echo "Veuillez confirmer votre compte, un email à été envoyé";
+                    }
                 } else {
                     // Mot de passe incorrect
                     echo "Mot de passe incorrect.";
